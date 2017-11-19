a956a6f97ec551168bc37d73f726cc6a8831310f
archer-bao <baoq1991@126.com>
2017-11-19 14:04:11 +0800

    Update main.go

--- main.go
+++ main.go
@@ -42,20 +42,21 @@ var f = retry()
 func retry() func() bool {
 	var retry int = 3
 	return func() bool {
+		time.Sleep(30 * time.Second)
 		if retry--; retry == 0 {
-			return false
-		} else {
 			return true
+		} else {
+			return false
 		}
 	}
 }
 
-func getItem() {
-	res, er := http.Get(`https://p.3.cn/prices/mgets?skuIds=J_1470147`)
+func getItem() (err error) {
+	res, err := http.Get(`https://p.3.cn/prices/mgets?skuIds=J_1470147`)
 
-	if er != nil {
+	if err != nil {
 		logger.Println("network error")
-		os.Exit(-1)
+		return
 	}
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
@@ -63,51 +64,61 @@ func getItem() {
 	if err != nil {
 		// handle error
 		if f() {
+			logger.Println("JD API CANNOT ACCESS (API接口无法访问)")
 			logger.Println("response:", string(body), " error: ", err)
-			sendSms("JD API CANNOT ACCESS (API接口无法访问)")
-			os.Exit(-1)
+			if sendSms("JD API CANNOT ACCESS (API接口无法访问)") != nil {
+				logger.Println("send SMS failed.")
+			}
 		}
+		return
 	} else {
 		result := string(body)
 		result = result[1 : len(result)-2]
 		var goods item
-		// print(result, "\n")
 
-		err := json.Unmarshal([]byte(result), &goods)
+		err = json.Unmarshal([]byte(result), &goods)
 		if err != nil {
+			logger.Println("JD API ERROR (API接口访问返回错误，可能需要验证码)")
 			logger.Println("response:", result, " error: ", err)
-			sendSms("JD API ERROR (API接口访问返回错误，可能需要验证码)")
-			os.Exit(-1)
-		} else {
-			if goods.P < 699 {
-				// print("promotion")
-				// print(strconv.FormatFloat(goods.P, 'f', 2, 32))
-				logger.Println("price:", goods.P)
-				sendSms("飞利浦（PHILIPS）行车记录仪ADR810已降价，当前价格:" + strconv.FormatFloat(goods.P, 'f', 2, 32))
+			if sendSms("JD API ERROR (API接口访问返回错误，可能需要验证码)") != nil {
+				logger.Println("send SMS failed.")
 			}
+			return
+		} else {
+			if goods.P == 699 {
+				logger.Println("price:", goods.P)
+				if sendSms("飞利浦（PHILIPS）行车记录仪ADR810已降价，当前价格:"+
+					strconv.FormatFloat(goods.P, 'f', 2, 32)) != nil {
+					logger.Println("send SMS failed.")
+				}
+			}
+			return nil
 		}
 	}
 }
 
-func sendSms(content string) {
+func sendSms(content string) (err error) {
 	client := &http.Client{}
 
-	req, _ := http.NewRequest("POST", "http://sms-api.luosimao.com/v1/send.json", strings.NewReader("mobile=13512345678&message="+content+"【sign】"))
+	req, _ := http.NewRequest("POST", "http://sms-api.luosimao.com/v1/send.json",
+		strings.NewReader("mobile=13512345678&message="+content+"【sign】"))
 
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Authorization", "Basic YXBpOmtleSr240stuek2budewtMjp9txt1c1hf3lxy3sjRlNw")
+	req.Header.Set("Authorization", "Basic YXBpOmtleSr240stuek2budewtMjp9txt1c1hf3lxy3sjRlNw==")
 
 	resp, err := client.Do(req)
 	if err != nil {
 		logger.Println("error:", err)
-		return
+		return err
 	}
 	defer resp.Body.Close()
 
-	body, err2 := ioutil.ReadAll(resp.Body)
-	if err2 != nil {
-		logger.Println("response:", string(body), " error: ", err2)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logger.Println("response:", string(body), " error: ", err)
+		return err
 	}
+	return nil
 }
 
 func GenerateRangeNum(min, max int) int {
@@ -128,7 +139,9 @@ func ServiceCheck() {
 func main() {
 	for {
 		ServiceCheck()
-		getItem()
+		if getItem() != nil {
+			logger.Println("getItem error")
+		}
 		time.Sleep(time.Duration(GenerateRangeNum(15, 25)) * time.Minute)
 	}
 }
